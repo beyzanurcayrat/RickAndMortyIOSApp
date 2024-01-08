fb076a1bd1be1a103cca3bf9408ba8b6ba30e9d3
beyzacayrat <beyzacayrat@gmail.com>
2024-01-08 18:32:38 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by beyzanur cayrat on 8.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
